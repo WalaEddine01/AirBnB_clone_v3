4b9803778fe3ad34492255a0ca334ce69542d0ea
walaeddine01 <walaaeddine33@gmail.com>
2024-02-03 20:23:12 +0100

    I made the changes on 2024-02-03 20:23:12

--- api/v1/views/users.py
+++ api/v1/views/users.py
@@ -5,7 +5,6 @@ Users view
 from flask import jsonify
 from api.v1.views import app_views
 from models import storage
-from models.state import State
 from models.user import User
 from flask import request, abort
 
