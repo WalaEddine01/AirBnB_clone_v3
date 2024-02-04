6cd632bd35c1c6cecf1010612099c56a8de8b098
walaeddine01 <walaaeddine33@gmail.com>
2024-02-04 13:21:34 +0100

    I made the changes on 2024-02-04 13:21:34

--- api/v1/views/places.py
+++ api/v1/views/places.py
@@ -3,7 +3,7 @@
 Place view
 """
 from api.v1.views import app_views
-from flask import abort, jsonify, request
+from flask import abort, jsonify, request, make_response
 from models import storage
 from models.state import State
 from models.user import User
@@ -11,7 +11,7 @@ from models.place import Place
 from models.city import City
 
 
-@app_views.route('/cities/<city_id>/places', methods=['GET'],
+@app_views.route('/cities/<string:city_id>/places', methods=['GET'],
                  strict_slashes=False)
 def get_place(city_id):
     '''
@@ -24,7 +24,7 @@ def get_place(city_id):
     return jsonify(place_list_dict)
 
 
-@app_views.route('/places/<place_id>', methods=['GET'],
+@app_views.route('/places/<string:place_id>', methods=['GET'],
                  strict_slashes=False)
 def get2_place(place_id):
     '''
@@ -36,7 +36,7 @@ def get2_place(place_id):
     return jsonify(place_dct.to_dict())
 
 
-@app_views.route('/places/<place_id>', methods=['DELETE'],
+@app_views.route('/places/<string:place_id>', methods=['DELETE'],
                  strict_slashes=False)
 def del_place(place_id):
     """
@@ -50,7 +50,7 @@ def del_place(place_id):
     abort(404)
 
 
-@app_views.route('/cities/<city_id>/places', methods=['POST'],
+@app_views.route('/cities/<string:city_id>/places', methods=['POST'],
                  strict_slashes=False)
 def post_place(city_id):
     """
@@ -58,20 +58,24 @@ def post_place(city_id):
     """
     json_data = request.get_json()
     if not json_data:
-        return "Not a JSON", 400
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     if "user_id" not in json_data:
-        return 'Missing user_id', 400
+        return make_response(jsonify({"error": "Missing user_id"}), 400)
     if 'name' not in json_data:
-        return "Missing name", 400
-    user_id = json_data.get('id')
+        return make_response(jsonify({"error": "Missing name"}), 400)
+    city = storage.get(City, city_id)
+    if city is None:
+        abort(404)
+    user = storage.get(User, json_data['user_id'])
+    if user is None:
+        abort(404)
     json_data['city_id'] = city_id
-    new = Place(**json_data)
-    storage.new(new)
-    storage.save()
-    return jsonify(new.to_dict()), 201
+    new_place = Place(**json_data)
+    new_place.save()
+    return jsonify(new_place.to_dict()), 201
 
 
-@app_views.route('/places/<place_id>', methods=['PUT'],
+@app_views.route('/places/<string:place_id>', methods=['PUT'],
                  strict_slashes=False)
 def put_place(place_id):
     """
@@ -82,9 +86,9 @@ def put_place(place_id):
     if not res:
         abort(404)
     if not json_data:
-        return 'Not a JSON', 400
+        return make_response(jsonify({"error": "Not a JSON"}), 400)
     for k, v in json_data.items():
-        if k not in ["id", "created_at", "updated_at", "user_id", "city_id"]:
+        if k not in ["id", "created_at", "updated_at"]:
             setattr(res, k, v)
     storage.save()
-    return jsonify(res.to_dict()), 200
+    return jsonify(res.to_dict())
