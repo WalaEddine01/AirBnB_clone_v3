96d36b1c1bc28d9aeb1b4e2c44ff7a8bbc580891
walaeddine01 <walaaeddine33@gmail.com>
2024-02-04 11:56:24 +0100

    I made the changes on 2024-02-04 11:56:24

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -79,10 +79,11 @@ class DBStorage:
         """
         this method retrieves one object
         """
-        if cls.__name__ in classes:
-            objs = self.__session.query(classes[cls.__name__]).\
-                filter(classes[cls.__name__].id == id).first()
-            return objs
+        if cls in classes.values() and id and type(id) is str:
+            d_obj = self.all(cls)
+            for key, value in d_obj.items():
+                if key.split(".")[1] == id:
+                    return value
         return None
 
     def count(self, cls=None):
