2ecb5e177af88f7f4f9c258ca2a12fb998592c81
walaeddine01 <walaaeddine33@gmail.com>
2024-02-05 22:24:06 +0100

    I made the changes on 2024-02-05 22:24:06

--- models/user.py
+++ models/user.py
@@ -5,6 +5,7 @@ from models.base_model import BaseModel, Base
 from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
+from hashlib import md5
 from sqlalchemy.orm import relationship
 
 
